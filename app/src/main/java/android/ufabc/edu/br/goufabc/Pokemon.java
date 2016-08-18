84f464afd3eb57c9b696e8afa706368e893c572c
guibegotti <domingos.guilherme@gmail.com>
2016-08-18 11:19:44 -0300

    Setting Pkmn Image again with URL but with another class

--- app/src/main/java/android/ufabc/edu/br/goufabc/Pokemon.java
+++ app/src/main/java/android/ufabc/edu/br/goufabc/Pokemon.java
@@ -3,13 +3,18 @@ package android.ufabc.edu.br.goufabc;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.AsyncTask;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class Pokemon extends AppCompatActivity {
@@ -37,21 +42,52 @@ public class Pokemon extends AppCompatActivity {
         id = Ide.intValue();
         TextView txtDetalhes = (TextView)findViewById(R.id.nomePokemon);
         txtDetalhes.setText(listaPokemon[id][0].toString()+" "+listaPokemon[id][1].toString());
+        String url = listaPokemon[id][2];
+        
+        new LoadImagefromUrl( ).execute((ImageView) findViewById(R.id.imgPkmn), listaPokemon[id][2].toString());
 
-        ImageView pkmnImage = (ImageView)findViewById(R.id.imgPkmn);
-        new ImageLoadTask(listaPokemon[id][2], pkmnImage).execute();
+        new LoadImagefromUrl( ).execute((ImageView) findViewById(R.id.imgType1), listaPokemon[id][3].toString());
 
-        ImageView tipeOneImage = (ImageView)findViewById(R.id.imgType1);
-        new ImageLoadTask(listaPokemon[id][3], pkmnImage).execute();
-
-        ImageView tipeTwoImage = (ImageView)findViewById(R.id.imgType2);
         if (listaPokemon[id][4] != "null"){
-            new ImageLoadTask(listaPokemon[id][4], pkmnImage).execute();
+            new LoadImagefromUrl( ).execute((ImageView) findViewById(R.id.imgType2), listaPokemon[id][4].toString());
         }
     }
 
     public void clickAdd(View view){
         Intent intent = new Intent(this, TimeActivity.class);
         startActivity(intent);
+    }
+
+    private class LoadImagefromUrl extends AsyncTask< Object, Void, Bitmap > {
+        ImageView ivPreview = null;
+
+        @Override
+        protected Bitmap doInBackground( Object... params ) {
+            this.ivPreview = (ImageView) params[0];
+            String url = (String) params[1];
+            System.out.println(url);
+            return loadBitmap( url );
+        }
+
+        @Override
+        protected void onPostExecute( Bitmap result ) {
+            super.onPostExecute( result );
+            ivPreview.setImageBitmap( result );
+        }
+    }
+
+    public Bitmap loadBitmap( String url ) {
+        URL newurl = null;
+        Bitmap bitmap = null;
+        try {
+            newurl = new URL( url );
+            bitmap = BitmapFactory.decodeStream( newurl.openConnection( ).getInputStream( ) );
+        } catch ( MalformedURLException e ) {
+            e.printStackTrace( );
+        } catch ( IOException e ) {
+
+            e.printStackTrace( );
+        }
+        return bitmap;
     }
 }
